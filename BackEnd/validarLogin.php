94c1c4f9ea75e2576d6f8f320e063f301d6c296b
5up3r22 <juandavidocampo80@gmail.com>
2023-02-15 00:33:42 -0500

    Update validarLogin.php

--- BackEnd/validarLogin.php
+++ BackEnd/validarLogin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$mysqli = new mysqli('mysql:host=containers-us-west-36.railway.app;port=3306;dbname=control_vet', 'root','YZv9j9VAjnUf4jAOpYvo');
+$mysqli = new mysqli('mysql://root:YZv9j9VAjnUf4jAOpYvo@containers-us-west-36.railway.app:7635/railway', 'root','YZv9j9VAjnUf4jAOpYvo');
 
 if ($mysqli->connect_errno) {
   echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
